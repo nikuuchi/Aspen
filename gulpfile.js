8f138e06e4859932ade803bcd399550fd8698f8c
uchida <uchida.atsushi.8@gmail.com>
2014-09-20 18:20:36 +0900

    fix directory

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var tsc   = require('gulp-tsc');
 var gjade = require('gulp-jade');
 
 gulp.task('default', function(){
-    gulp.src(['app.ts'])
+    gulp.src(['server.ts'])
         .pipe(tsc())
         .pipe(gulp.dest("."));
     //gulp.src(['jade/index.jade', 'jade/editor.jade'])
