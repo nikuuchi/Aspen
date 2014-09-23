fb2a77a4ef1d4dafba99b440619f7bedbe4dcc31
uchida <uchida.atsushi.8@gmail.com>
2014-09-24 02:36:22 +0900

    remove users.js and add ORM

--- server.ts
+++ server.ts
@@ -11,7 +11,6 @@ var bodyParser = require('body-parser');
 var config = require('config');
 
 var routes = require('./app/routes/index');
-var users = require('./app/routes/users');
 
 var app = express();
 
@@ -27,7 +26,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
