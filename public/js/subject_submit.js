b99ca260f2651a68d0779a66983bbb1a4d3e3071
uchida <uchida.atsushi.8@gmail.com>
2014-10-03 16:28:57 +0900

    add hidden form

--- public/js/subject_submit.js
+++ public/js/subject_submit.js
@@ -5,8 +5,15 @@ $(function () {
     editor = ace.edit("editor");
     editor.setTheme("ace/theme/xcode");
     editor.getSession().setMode("ace/mode/c_cpp");
-
-    //(<any>editor).setFontSize(14);
-    function postData(data) {
-    }
+    editor.setFontSize(14);
 });
+
+function postData() {
+    var form = document.getElementById("subject_form");
+    var hidden_element = document.createElement("input");
+    hidden_element.setAttribute("name", "content");
+    hidden_element.setAttribute("type", "hidden");
+    hidden_element.setAttribute("value", editor.getValue());
+    form.appendChild(hidden_element);
+    form.submit();
+}
