55376830af10efc1030670fdc16a87b338bccfbc
uchida <uchida.atsushi.8@gmail.com>
2014-10-03 16:10:20 +0900

    remove unused lib

---
+++ public/js/subject_submit.ts
@@ -0,0 +1,14 @@
+///<reference path="../../typings/jquery/jquery.d.ts" />
+///<reference path="../../typings/ace/ace.d.ts" />
+
+var editor;
+$(function() {
+    editor = ace.edit("editor");
+    editor.setTheme("ace/theme/xcode");
+    editor.getSession().setMode("ace/mode/c_cpp");
+    //(<any>editor).setFontSize(14);
+    function postData(data) {
+
+    }
+});
+
