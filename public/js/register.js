d0620ec6c3a5a78183aaf2c50427ba6be6ee512a
takersu <takeru.sudo.at@gmail.com>
2014-10-20 18:12:44 +0900

    fix replace

--- public/js/register.js
+++ public/js/register.js
@@ -2,10 +2,10 @@ $(function() {
   $("input").on("input", function() {
     $("button").attr("disabled", "");
     $("button").addClass("disabled");
-    var snum = $("input[name='inputNumber']").val().toUpperCase();
+    var snum = $("input[name='inputNumber']").val().toLowerCase();
     snum = snum.replace(/^B/,"b");
-    $("input[name='inputNumber']").val(snum);
-    if(snum.match(/b[0-9]{2}[0-9A-Z].[0-9]{3}$/)) {
+    if(snum.match(/b[0-9]{2}[0-9a-z].[0-9]{3}$/)) {
+      $("input[name='inputNumber']").val(snum);
       if($("input[name='inputName']").val() !== "") {
         $("button").removeClass("disabled");
         $("button").removeAttr("disabled");
