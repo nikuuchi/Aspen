6c8ba5e182be16a50b43c3836943a520e77f3b81
takersu <takeru.sudo.at@gmail.com>
2014-10-20 16:10:14 +0900

    fix validation

--- public/js/register.js
+++ public/js/register.js
@@ -2,7 +2,10 @@ $(function() {
   $("input").on("input", function() {
     $("button").attr("disabled", "");
     $("button").addClass("disabled");
-    if($("input[name='inputNumber']").val().match(/b[0-9]{7}$/)) {
+    var snum = $("input[name='inputNumber']").val().toUpperCase();
+    snum = snum.replace(/^B/,"b");
+    $("input[name='inputNumber']").val(snum);
+    if(snum.match(/b[0-9]{2}[0-9A-Z].[0-9]{3}$/)) {
       if($("input[name='inputName']").val() !== "") {
         $("button").removeClass("disabled");
         $("button").removeAttr("disabled");
