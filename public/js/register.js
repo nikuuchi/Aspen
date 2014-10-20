121dd7f041f8de3a1455a20d4fcd428027d338b3
takersu <takeru.sudo.at@gmail.com>
2014-10-20 18:15:10 +0900

    fix

--- public/js/register.js
+++ public/js/register.js
@@ -3,7 +3,6 @@ $(function() {
     $("button").attr("disabled", "");
     $("button").addClass("disabled");
     var snum = $("input[name='inputNumber']").val().toLowerCase();
-    snum = snum.replace(/^B/,"b");
     if(snum.match(/b[0-9]{2}[0-9a-z].[0-9]{3}$/)) {
       $("input[name='inputNumber']").val(snum);
       if($("input[name='inputName']").val() !== "") {
