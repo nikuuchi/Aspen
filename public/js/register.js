25bed4cffa686abf30e0f70972131494739e3075
takersu <takeru.sudo.at@gmail.com>
2014-10-22 15:15:36 +0900

    fix validation regexp

--- public/js/register.js
+++ public/js/register.js
@@ -3,13 +3,15 @@ $(function() {
     $("button").attr("disabled", "");
     $("button").addClass("disabled");
     var snum = $("input[name='inputNumber']").val().toLowerCase();
-    if(snum.match(/b[0-9]{2}[0-9a-z].[0-9]{3}$/)) {
-      $("input[name='inputNumber']").val(snum);
+    if(snum.match(/^[a-z][0-9]{2}[0-9a-z].[0-9]{3}$/)) {
       if($("input[name='inputName']").val() !== "") {
         $("button").removeClass("disabled");
         $("button").removeAttr("disabled");
       }
     }
+  });
+  $("input[name='inputNumber']").blur(function() {
+    $(this).val($(this).val().toLowerCase());
   });
 
 });
